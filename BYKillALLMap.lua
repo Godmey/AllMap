12bf96cc9a5ac48a0e84104e8b0066733ad67cd8
Godmey <166684533+Godmey@users.noreply.github.com>
2024-08-18 19:35:08 +0700

    Update BYKillALLMap.lua

--- BYKillALLMap.lua
+++ BYKillALLMap.lua
@@ -1,8 +1,7 @@
 local ID_MAP = tostring(game.PlaceId)
--- setclipboard(ID_MAP)
 local CheckMap = {
-  ["Map: Glue Piece"] = 394506555,
-  ["Map: XDAX Piece"] = 17415731457,
+  ["Map: Glue Piece"] = "394506555",
+  ["Map: XDAX Piece"] = "17415731457",
   ["Map 3"] = nil,
   ["Map 4"] = nil,
   ["Map 5"] = nil,
@@ -11,13 +10,9 @@ local CheckMap = {
 local HTTP
 if CheckMap["Map: Glue Piece"] == ID_MAP then
   HTTP = "https://raw.githubusercontent.com/Godmey/Glue-piece-By-Kill/main/README.md"
-  
 elseif CheckMap["Map: XDAX Piece"] == ID_MAP then
   HTTP = "https://raw.githubusercontent.com/Godmey/XDAX/main/Piece.lua"
-  
 end
-
-
 
 if HTTP then
   loadstring(game:HttpGet(HTTP))()
